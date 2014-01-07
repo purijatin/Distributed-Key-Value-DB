d0cfa54c02ffc46372b406f5cd7808755bbfd851
Jatin <purijatin@gmail.com>
2014-01-07 17:56:44 +0530

    Improved Documentation

--- src/main/scala/sample/HelloWorld.scala
+++ src/main/scala/sample/HelloWorld.scala
@@ -9,6 +9,11 @@ import kvstore.Replica._
 import akka.actor.ActorSystem
 import akka.actor.Props
 
+/**
+ * Hello World example:
+ * It creates a arbiter with one Primary Node
+ *
+ */
 object HelloWorld {
   def main(args: Array[String]) {
     val system = ActorSystem("Main")
@@ -20,17 +25,20 @@ object HelloWorld {
 
 class Main2(arbiter: ActorRef) extends Actor {
   import kvstore.Arbiter._
-
+  /**
+   * On doing below, the replica will automatically send a call to arbiter to add itself in the cluster.
+   * Because this is the first actor to Join arbiter, hence it becomes the Primary Node 
+   */
   val replica = context.actorOf(Replica.props(arbiter, Persistence.props(flaky = false)))
 
   def receive = {
     case "send" =>
-      replica ! Insert("a", "a", 0)
-      replica ! Get("a", 1)
-    case GetResult(key, opt, id) => 
+      replica ! Insert("a", "a", 0) //send an Insert message
+      replica ! Get("a", 1) //Get the value of key "a"
+    case GetResult(key, opt, id) =>
       println(s"$self GetResult: ($key, $opt)")
       context.system.shutdown()
-    case x @ _ => println(x)
+    case x @ _ => println(x) //Any Acknowledgement for Insert in this case
   }
 
 }
